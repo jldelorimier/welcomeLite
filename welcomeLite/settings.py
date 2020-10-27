d2172ff910bf46c89aac5f5e45216bb50c5f05c0
Julia de Lorimier <jldelorimier@gmail.com>
2020-10-26 19:40:51 -0700

    Defined Offer model in models.py including foreign key; imported Django authentication system in models.py for User to be used by foreign key; Added WelcomeLiteApp to installed apps in setting.py; migrated offer model.

--- welcomeLite/settings.py
+++ welcomeLite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'welcomeLiteApp.apps.WelcomeliteappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- welcomeLiteApp/models.py
+++ welcomeLiteApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User # < Importing Django's built-in User model that provides us with username, email, password, first_name, and last_name fields. Based this on https://learndjango.com/tutorials/django-best-practices-referencing-user-model
+
+class Offer(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  candidate_first_name = models.CharField(max_length=200)
+  candidate_last_name = models.CharField(max_length=200)
+  candidate_email_address = models.CharField(max_length=200)
+  role = models.CharField(max_length=200)
+  salary = models.IntegerField(default=0)
 
 # Create your models here.
