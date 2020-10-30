b49486b092d5028dac094186f9d42e27f77c3555
Julia de Lorimier <jldelorimier@gmail.com>
2020-10-29 18:56:21 -0700

    Added index method in views.py; added index url under welcomeLiteApp/urls.py; created index.html and new.html files; added form to new.html and simple href link to add a new offer on index.html

--- welcomeLiteApp/models.py
+++ welcomeLiteApp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User # < Importing Django's built-in User model that provides us with username, email, password, first_name, and last_name fields. Based this on https://learndjango.com/tutorials/django-best-practices-referencing-user-model
 
+# class User(models.Model):
+#   username = models.CharField(max_length=200)
+
 class Offer(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   candidate_first_name = models.CharField(max_length=200)
@@ -8,5 +11,9 @@ class Offer(models.Model):
   candidate_email_address = models.CharField(max_length=200)
   role = models.CharField(max_length=200)
   salary = models.IntegerField(default=0)
+  # salary_string = str(salary)
+  def __str__(self):
+    return self.candidate_first_name + ", " + self.candidate_last_name + ", " + self.candidate_email_address + ", " + self.role
+    #  + ", " + self.salary_string  <<< This was giving me back <django.db.models.fields.IntegerField> in the interactive console
 
 # Create your models here.
--- welcomeLiteApp/views.py
+++ welcomeLiteApp/views.py
@@ -1,5 +1,22 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello, world. You're at the welcomeLiteApp index.")
+
+def create(request):
+  if request.method == 'POST':
+    # populate the form with the input details
+    offer = OfferForm(request.POST)
+    if offer.is_valid():
+        offer.save()
+        return redirect('index')
+  else:
+    # display a new form again
+    offer = OfferForm()
+
+  context = {
+    'offer': offer
+  }
+
+  return render(request, 'new.html', context)
